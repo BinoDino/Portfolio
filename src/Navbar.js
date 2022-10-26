f3ebc2332805c2c1053ca0df6c0c96769b6bee1d
Sabine Britner <sabine@Sabines-Air.zyxel>
2022-10-26 10:06:27 +0200

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Navbar extends React.Component{
+ render(){
+    return <ul className='nav flex-column'>
+                <li class="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled">Disabled</a>
+                </li>
+           </ul>
+ }   
+}
+
+export default Navbar;
