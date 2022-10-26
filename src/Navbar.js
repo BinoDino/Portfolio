2e6043af0f0fda003dbbe88ee988b253c70f21fc
Sabine Britner <sabine@Sabines-Air.zyxel>
2022-10-26 18:45:32 +0200

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,55 @@
 import React from 'react';
+import './Navbar.css';
+import {ReactComponent as Avatar} from './img/user.svg';
+import {ReactComponent as CV} from './img/cv.svg';
+import {ReactComponent as Code} from './img/code.svg';
+import {ReactComponent as Contact} from './img/contact.svg';
+import {ReactComponent as Credits} from './img/link.svg';
+
+// vertical navbar. inspired by https://www.youtube.com/watch?v=1uEAt51fEos 
+
+class NavbarElement extends React.Component{
+    render(){
+        return <div key={'navItem#'+this.props.keyVal} className='d-flex align-items-center flex-nowrap'>
+                    <div key={'navItem#'+this.props.keyVal+'.1'} className="p-2">
+                        <a href={this.props.href}>
+                            {this.props.children}
+                        </a>
+                    </div>
+                    <div key={'navItem#'+this.props.keyVal+'.2'} className="p-2">
+                        <a href={this.props.href}>
+                            <span className='nav-text'>{this.props.text}</span>
+                        </a>
+                    </div>
+                </div>
+    }
+}
 
 class Navbar extends React.Component{
+ state={
+    active: false,
+    icons:[<Avatar/>,<CV/>,<Code/>,<Contact/>,<Credits/>],
+    text:['Profil', 'Lebenslauf', 'Projekte', 'Kontakt', 'Credits'],
+    href:['#','#','#','#','#'] 
+ }
+ handleClick = () => {
+    this.setState({
+        active: !this.state.active
+  })
+ }
  render(){
-    return <ul className='nav flex-column'>
-                <li class="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Active</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled">Disabled</a>
-                </li>
-           </ul>
+    var navElements =[];
+    for(let i=0; i<this.state.icons.length; i++){
+        navElements.push(<NavbarElement keyVal={i.toString()} href={this.state.href[i]} text={this.state.text[i]}>{this.state.icons[i]}</NavbarElement>);
+    }
+    return <div className={'nav-container' + (this.state.active ? ' active': '') } >
+                <a href='#' className={'toggleBox' + (this.state.active ? ' active': '') } onClick = {this.handleClick}>
+                    <span className='icon'></span>
+                </a>
+                <div className={'menu' + (this.state.active ? ' active': '')}>
+                    {navElements}
+                </div>
+            </div>
  }   
 }
 
